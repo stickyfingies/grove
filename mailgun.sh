86762cea47557c4caea56c01626d3e3304f85a0b
Seth T.  Traman <celltheman123@gmail.com>
2017-05-08 13:18:37 +0000

    general_updates

---
+++ mailgun.sh
@@ -0,0 +1,6 @@
+curl -s --user 'api:key-6e8a84deecf885b3320ea74bfe17607b' \
+https://api.mailgun.net/v3/mg.grove-mmo.com/messages \  
+-F to=nathen418@icloud.com \
+-F from='The Grove Team <noreply@grove-mmo.com>' \                                                                      
+-F subject='Welcome to The Grove' \
+-F text='Welcome to The Grove. Through this list you will be notified about changes in The Grove. If you have any questions we would be happy to help. --The Grove Team'
