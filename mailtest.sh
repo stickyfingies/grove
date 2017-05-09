07630ff3a61bcab20dd4b32aec3a7bc9a1fcbec5
Seth T.  Traman <celltheman123@gmail.com>
2017-05-09 14:22:01 +0000

    eneral_updates

--- mailtest.sh
+++ mailtest.sh
@@ -1,3 +1,3 @@
-curl -s --user 'api:key-6e8a84deecf885b3320ea74bfe17607b' https://api.mailgun.net/v3/mg.grove-mmo.com/messages -F to=conwater@gmail.com -F from='The Grove Team <noreply@mg.gove-mmo.com>' -F subject='Welcome to The Grove' -F text='Welcome to The Grove. Through this list you will be notified about changes in The Grove. If you have any questions we would be happy to help. --The Grove Team grove-mmo.com '
+curl -s --user 'api:key-6e8a84deecf885b3320ea74bfe17607b' https://api.mailgun.net/v3/mg.grove-mmo.com/messages -F to= -F from='The Grove Team <noreply@mg.gove-mmo.com>' -F subject='Welcome to The Grove' -F text='Welcome to The Grove. Through this list you will be notified about changes in The Grove. If you have any questions we would be happy to help. --The Grove Team grove-mmo.com '
 
 #replace amail address and text with desired stuff
