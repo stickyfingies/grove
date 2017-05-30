1694f8a10dce61d49741df9a2bb28a5eb0c32937
Seth T.  Traman <celltheman123@gmail.com>
2017-05-30 00:12:07 +0000

    general_updates

--- mail.sh
+++ mail.sh
@@ -1,3 +1,3 @@
-curl -s --user 'api:key-6e8a84deecf885b3320ea74bfe17607b' https://api.mailgun.net/v3/mg.grove-mmo.com/messages -F to='The Grove Team <noreply@mg.gove-mmo.com>' -F from='app0aa9341b03e649a9839c0825dd5ca821.mailgun.org' -F subject='' -F text=''
+curl -s --user 'api:key-6e8a84deecf885b3320ea74bfe17607b' https://api.mailgun.net/v3/mg.grove-mmo.com/messages -F to='kailapuppy@me.com' -F from='app0aa9341b03e649a9839c0825dd5ca821.mailgun.org' -F subject='BOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!!!!!!!!' -F text='BOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!!!'
 
 #replace amail address and text with desired stuff.
