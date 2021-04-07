8e1e7ec7b021d64a828f5e5478c01832dd4817a1
StickyFingies <sethtraman@gmail.com>
2021-04-07 11:40:55 -0500

    Fix security vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,72 @@
-"use strict";
-
-import path from "path";
-import webpack from "webpack";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-
-let last = "";
+import path from 'path';
+import webpack from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 export default {
-    mode: "development",
-    name: "js",
-    entry: {
-        main: "./src/js/main.ts",
+  mode: 'development',
+  name: 'js',
+  entry: {
+    main: './src/js/main.ts',
+  },
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[chunkhash].js',
+    path: path.resolve('static/js/dist'),
+  },
+  watchOptions: {
+    ignored: ['**/node_modules', 'server/'],
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader?url=false',
+      ],
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      resolve: {
+        fullySpecified: false,
+      },
+    }, {
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true,
+      },
+    }],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      hidePathInfo: true,
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true,
+        },
+      },
     },
-    output: {
-        filename: "[name].js",
-        chunkFilename: "[chunkhash].js",
-        path: path.resolve("static/js/dist")
-    },
-    watchOptions: {
-        ignored: ["**/node_modules", "server/"]
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [
-                "style-loader",
-                "css-loader?minimize=true"
-            ]
-        }, {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            resolve: {
-                fullySpecified: false
-            }
-        }, {
-            test: /\.ts$/,
-            exclude: /node_modules/,
-            loader: "ts-loader",
-            options: {
-                transpileOnly: true
-            }
-        }],
-    },
-    optimization: {
-        runtimeChunk: "single",
-        splitChunks: {
-            chunks: "all",
-            hidePathInfo: true,
-            cacheGroups: {
-                vendor: {
-                    test: /node_modules/,
-                    chunks: "initial",
-                    name: "vendor",
-                    enforce: true
-                }
-            }
-        }
-    },
-    plugins: [
-        new webpack.BannerPlugin("\nMade with <3 by the Grove team | " + new Date() + "\n"),
-        new ForkTsCheckerWebpackPlugin({
-            typescript: {
-                diagnosticOptions: {
-                    semantic: true,
-                    syntactic: true,
-                },
-            },
-        })
-    ],
-    resolve: {
-        extensions: [".ts", ".js", ".json", ".css"]
-    }
+  },
+  plugins: [
+    new webpack.BannerPlugin(`\nMade with <3 by the Grove team | ${new Date()}\n`),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true,
+        },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.json', '.css'],
+  },
 };
 
 //
